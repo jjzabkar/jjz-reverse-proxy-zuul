642fa9939ab47386559510635d3cbac2825961b5
JJ Zabkar <jjzabkar@gmail.com>
2016-01-19 22:05:56 -0800

    initial commit generated from Spring Initializr

---
+++ src/main/java/com/jjz/zuul/JjzReverseProxyZuulApplication.java
@@ -0,0 +1,12 @@
+package com.jjz.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjzReverseProxyZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjzReverseProxyZuulApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jjz/zuul/JjzReverseProxyZuulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jjz.zuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = JjzReverseProxyZuulApplication.class)
+public class JjzReverseProxyZuulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
